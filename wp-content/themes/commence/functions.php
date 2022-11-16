ae2fa7f2161677218187ad9b3d59f8e432a676d8
Jose Rios <jose@commence.studio>
2022-11-15 16:47:04 -0800

    :rocket: Add merch post type

--- wp-content/themes/commence/functions.php
+++ wp-content/themes/commence/functions.php
@@ -103,7 +103,7 @@ function create_beers_cpt()
     'label' => __('Beer', 'textdomain'),
     'description' => __('', 'textdomain'),
     'labels' => $labels,
-    'menu_icon' => 'dashicons-coffee',
+    'menu_icon' => 'dashicons-beer',
     'supports' => array('title', 'excerpt', 'thumbnail'),
     'public' => true,
     'show_ui' => true,
@@ -121,4 +121,62 @@ function create_beers_cpt()
     'taxonomies' => array('post_tag', 'category'),
   );
   register_post_type('beer', $args);
+}
+
+// Register Custom Post Type Merch
+// Post Type Key: merch
+add_action('init', 'create_merch_cpt', 0);
+function create_merch_cpt()
+{
+  $labels = array(
+    'name' => __('Merch', 'Post Type General Name', 'textdomain'),
+    'singular_name' => __('Merch', 'Post Type Singular Name', 'textdomain'),
+    'menu_name' => __('Merch', 'textdomain'),
+    'name_admin_bar' => __('Merch', 'textdomain'),
+    'archives' => __('Merch Archives', 'textdomain'),
+    'attributes' => __('Merch Attributes', 'textdomain'),
+    'parent_item_colon' => __('Parent Merch:', 'textdomain'),
+    'all_items' => __('All Merch', 'textdomain'),
+    'add_new_item' => __('Add New Merch Item', 'textdomain'),
+    'add_new' => __('Add New', 'textdomain'),
+    'new_item' => __('New Merch Item', 'textdomain'),
+    'edit_item' => __('Edit Merch Item', 'textdomain'),
+    'update_item' => __('Update Merch Item', 'textdomain'),
+    'view_item' => __('View Merch Item', 'textdomain'),
+    'view_items' => __('View Merch', 'textdomain'),
+    'search_items' => __('Search Merch', 'textdomain'),
+    'not_found' => __('Not found', 'textdomain'),
+    'not_found_in_trash' => __('Not found in Trash', 'textdomain'),
+    'featured_image' => __('Featured Image', 'textdomain'),
+    'set_featured_image' => __('Set featured image', 'textdomain'),
+    'remove_featured_image' => __('Remove featured image', 'textdomain'),
+    'use_featured_image' => __('Use as featured image', 'textdomain'),
+    'insert_into_item' => __('Insert into Merch Item', 'textdomain'),
+    'uploaded_to_this_item' => __('Uploaded to this Merch Item', 'textdomain'),
+    'items_list' => __('Merch list', 'textdomain'),
+    'items_list_navigation' => __('Merch list navigation', 'textdomain'),
+    'filter_items_list' => __('Filter Merch list', 'textdomain'),
+  );
+  $args = array(
+    'label' => __('Merch', 'textdomain'),
+    'description' => __('', 'textdomain'),
+    'labels' => $labels,
+    'menu_icon' => 'dashicons-store',
+    'supports' => array('title', 'excerpt', 'thumbnail'),
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'menu_position' => 4,
+    'show_in_admin_bar' => true,
+    'show_in_nav_menus' => true,
+    'can_export' => true,
+    'has_archive' => false,
+    'hierarchical' => false,
+    'exclude_from_search' => false,
+    'show_in_rest' => true,
+    'publicly_queryable' => true,
+    'capability_type' => 'post',
+    'taxonomies' => array('post_tag', 'category'),
+  );
+  register_post_type('merch', $args);
 }
