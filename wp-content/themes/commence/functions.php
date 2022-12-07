4e5049f8703afb4290c76eb587e9cab2bcf99e6a
Jose Rios <jose@commence.studio>
2022-12-07 00:16:52 -0800

    :rocket: Remove plugins/add js file

--- wp-content/themes/commence/functions.php
+++ wp-content/themes/commence/functions.php
@@ -7,6 +7,15 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_uri() );
 }
 
+/* Custom script with no dependencies, enqueued in the footer */
+
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_scripts');
+function  my_theme_enqueue_scripts() {
+    wp_enqueue_script('custom', get_stylesheet_directory_uri().'/js/theme.js', 
+    array(), false, true);
+}
+
+
 // Register Custom Post Type Artist
 // Post Type Key: artist
 add_action('init', 'create_artists_cpt', 0);
