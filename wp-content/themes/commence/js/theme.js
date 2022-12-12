42f92e5e0a9b9ba180651c626ee33c67365c77e0
Jose Rios <jose@commence.studio>
2022-12-11 22:19:44 -0800

    :construction: Eyeball animation

--- wp-content/themes/commence/js/theme.js
+++ wp-content/themes/commence/js/theme.js
@@ -6,11 +6,12 @@ document.addEventListener("DOMContentLoaded", function() {
   container.addEventListener("mousemove", (e) => {
   const eyes = document.querySelectorAll(".home .age-gate__additional-information p img:nth-child(2)");
   [].forEach.call(eyes, function (eye) {
-    let mouseX = eye.getBoundingClientRect().right;
-    let mouseY = eye.getBoundingClientRect().top;
-    let radianDegrees = Math.atan2(e.pageX - mouseX, e.pageY - mouseY);
-    let rotationDegrees = radianDegrees * (180 / Math.PI) * -1 + 180;
-    eye.style.transform = `rotate(${rotationDegrees}deg)`;
+    window.addEventListener('mousemove', (evt) => {
+      const x = -(window.innerWidth / 2 - evt.pageX) / 50;
+      const y = -(window.innerHeight / 2 - evt.pageY) / 20;
+      eye.style.transform = `translateY(${y}px) translateX(${x}px)`;
+
+    });
   });
 });
 });
