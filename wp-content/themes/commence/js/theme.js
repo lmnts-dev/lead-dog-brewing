a32c97ceeb7ec7a89fd3db0ef166e9b45181eee2
Jose Rios <jose@commence.studio>
2022-12-12 14:32:15 -0800

    :construction: Beer drawer

--- wp-content/themes/commence/js/theme.js
+++ wp-content/themes/commence/js/theme.js
@@ -4,21 +4,24 @@ document.addEventListener("DOMContentLoaded", function() {
 //FOR THE EYEBALL ANIMATION
 	const container = document.querySelector(".home .age-gate__wrapper");
   container.addEventListener("mousemove", (e) => {
-  const eyes = document.querySelectorAll(".home .age-gate__additional-information p img:nth-child(2)");
-  [].forEach.call(eyes, function (eye) {
-    window.addEventListener('mousemove', (evt) => {
-      const x = -(window.innerWidth / 2 - evt.pageX) / 50;
-      const y = -(window.innerHeight / 2 - evt.pageY) / 50;
-      eye.style.transform = `translateY(${y}px) translateX(${x}px)`;
+    const eyes = document.querySelectorAll(".home .age-gate__additional-information p img:nth-child(2)");
+    [].forEach.call(eyes, function (eye) {
+      window.addEventListener('mousemove', (evt) => {
+        const x = -(window.innerWidth / 2 - evt.pageX) / 50;
+        const y = -(window.innerHeight / 2 - evt.pageY) / 50;
+        eye.style.transform = `translateY(${y}px) translateX(${x}px)`;
 
+      });
     });
   });
-  });
-  
-  //Beer Dropdown
-  let beerLink = document.querySelector('.beer--mobile-link');
-  console.log(beerLink, 'link here');
 });
 
+  //Beer Dropdown
+  let beerLink = document.querySelector('.beer--mobile-link');
+  beerLink.addEventListener("click", myFunction);
+  console.log(beerLink, 'link here');
 
+function myFunction() {
+    console.log(beerLink, 'link here');
+}
 
